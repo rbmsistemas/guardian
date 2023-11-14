a6baa341eccbaca0c65c9d72f40c811dbfb283c6
RaulBellosoM <raul.belloso.m@gmail.com>
2023-11-14 13:30:27 -0600

    fix recepcion date

--- src/pages/inventory/InventoryForm.jsx
+++ src/pages/inventory/InventoryForm.jsx
@@ -144,7 +144,11 @@ const InventoryForm = () => {
           });
         }
 
-        if (data.recepcionDate) {
+        if (
+          data.recepcionDate !== null &&
+          data.recepcionDate !== undefined &&
+          data.recepcionDate !== ""
+        ) {
           setData({
             ...data,
             recepcionDate: new Date(data.recepcionDate),
@@ -222,7 +226,11 @@ const InventoryForm = () => {
           altaDate: getCurrentFormattedDate(),
         };
 
-        if (data.recepcionDate.length > 0) {
+        if (
+          data.recepcionDate !== null &&
+          data.recepcionDate !== undefined &&
+          data.recepcionDate !== ""
+        ) {
           sendData = {
             ...sendData,
             recepcionDate: new Date(data.recepcionDate),
